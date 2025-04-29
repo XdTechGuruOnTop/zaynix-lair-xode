83986d5cb39f8899b8880799eba3c82fc052d83a
XdTechDev <arohiraju238@gmail.com>
2025-04-29 08:27:30 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,27 +1,53 @@
 const mega = require("megajs");
+
 const auth = {
     email: 'romekxd8@gmail.com',
     password: 'g64GbnXm.Y3a8F8',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
+};
+
+function randomMegaId(length = 6, numberLength = 4) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    const number = Math.floor(Math.random() * Math.pow(10, numberLength));
+    return `${result}${number}`;
 }
 
-const upload = (data, name) => {
+async function uploadToMega(filePath) {
     return new Promise((resolve, reject) => {
         try {
-            const storage = new mega.Storage(auth, () => {
-                data.pipe(storage.upload({name: name, allowUploadBuffering: true}));
-                storage.on("add", (file) => {
+            const storage = new mega.Storage(auth, (err) => {
+                if (err) return reject(err);
+                
+                const fileName = `${randomMegaId()}.json`;
+                const uploadStream = storage.upload({
+                    name: fileName,
+                    allowUploadBuffering: true
+                }, fs.createReadStream(filePath));
+                
+                uploadStream.on("complete", (file) => {
                     file.link((err, url) => {
-                        if (err) throw err;
-                        storage.close()
+                        if (err) return reject(err);
+                        storage.close();
                         resolve(url);
                     });
+                });
+                
+                uploadStream.on("error", (err) => {
+                    storage.close();
+                    reject(err);
                 });
             });
         } catch (err) {
             reject(err);
         }
     });
-};
+}
 
-module.exports = { upload };
+module.exports = {
+    uploadToMega,
+    randomMegaId
+};
